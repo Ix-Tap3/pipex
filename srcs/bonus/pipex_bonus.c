eb8d09e748c4d1335a13d173d2f7342076fbcfe8
pcaplat <patxicaplat.pro@gmail.com>
2026-01-23 10:05:12 +0100

    correcting bad fds management

--- srcs/bonus/pipex_bonus.c
+++ srcs/bonus/pipex_bonus.c
@@ -6,28 +6,27 @@
 /*   By: pcaplat <pcaplat@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 20:18:27 by pcaplat           #+#    #+#             */
-/*   Updated: 2026/01/21 22:04:08 by pcaplat          ###   ########.fr       */
+/*   Updated: 2026/01/23 10:03:35 by pcaplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/pipex_bonus.h"
 
-static void	close_fds(t_pipex *data)
-{
-	close(data->in_fd);
-	close(data->p_fd[0]);
-	close(data->p_fd[1]);
-	close(data->out_fd);
-	if (data->prev_fd != -1)
-		close(data->prev_fd);
-}
+//static void	close_fds(t_pipex *data)
+//{
+//	close(data->in_fd);
+//	close(data->p_fd[0]);
+//	close(data->p_fd[1]);
+//	close(data->out_fd);
+//	if (data->prev_fd != -1)
+//		close(data->prev_fd);
+//}
 
 static void	wait_all(t_pipex *data)
 {
 	int	i;
 	int	status;
 
-	close_fds(data);
 	status = 0;
 	i = 0;
 	while (i < data->cmd_count)
@@ -42,14 +41,26 @@ static int	child_process(t_pipex *data, t_list *cmds, int i)
 	char	*path;
 
 	if (i == 0)
+	{
 		dup2(data->in_fd, STDIN_FILENO);
+		close(data->in_fd);
+	}
 	else
+	{
 		dup2(data->prev_fd, STDIN_FILENO);
+		close(data->prev_fd);
+	}
 	if (i == data->cmd_count - 1)
+	{
 		dup2(data->out_fd, STDOUT_FILENO);
+		close(data->out_fd);
+	}
 	else
+	{
 		dup2(data->p_fd[1], STDOUT_FILENO);
-	close_fds(data);
+		close(data->p_fd[1]);
+	}
+	//close_fds(data);
 	path = parse_path(data->ev, ((char **)cmds->content));
 	if (!path)
 		return (-1);
@@ -89,6 +100,8 @@ int	pipex(t_pipex *data)
 		cmds = cmds->next;
 		i++;
 	}
+	close(data->in_fd);
+	close(data->out_fd);
 	wait_all(data);
 	return (1);
 }
